70ec4f7125bd694d57a3255093868efc06fffbfe
j-smit <jeroen-git@boffinhouse.com>
2015-11-27 15:08:26 +0100

    updated project

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,10 @@
 	'use strict';
 
 	module.exports = function (grunt) {
+		grunt.task.loadNpmTasks('grunt-auto-install');
+		grunt.task.loadNpmTasks('grunt-scssglobbing');
 		grunt.task.loadNpmTasks('grunt-sass');
 		grunt.task.loadNpmTasks('grunt-jsdoc');
-		grunt.task.loadNpmTasks('grunt-scssglobbing');
-		grunt.task.loadNpmTasks('grunt-contrib-copy');
 		grunt.task.loadNpmTasks('grunt-contrib-clean');
 		grunt.task.loadNpmTasks('grunt-autoprefixer');
 		grunt.task.loadNpmTasks('grunt-contrib-watch');
@@ -13,16 +13,25 @@
 		grunt.task.loadNpmTasks('assemble');
 
 		grunt.initConfig({
+			auto_install: {
+				subdir: {
+					options: {
+						bower: 'false'
+					}
+				}
+			},
 			sass: {
 				options: {
 					outputStyle: 'nested',
 					sourceMap: false
 				},
 				dist: {
-					files: {
-						'dist/css/styles.css': 'component-helpers/sass/tmp_styles.scss'
-					}
-				},
+					files: [
+						{
+							'dist/css/styles.css': 'component-helpers/sass/tmp_styles.scss',
+						}
+					]
+				}
 			},
 			clean: {
 				options: {
@@ -45,7 +54,7 @@
 				scssglobbing: {
 					files: [
 						{
-							src: ['component-helpers/sass/tmp_*.scss']
+							src: ['component-helpers/sass/tmp_styles.scss']
 						}
 					]
 				}
@@ -61,9 +70,19 @@
 					src: 'dist/css/*.css'
 				}
 			},
+			jshint: {
+				options: {
+					jshintrc: true
+				},
+				js: {
+					files: {
+						src: ['sources/components/**/*.js', 'tests/**/*.js']
+					}
+				}
+			},
 			jsdoc: {
 				dist : {
-					src: ['sources/**/*.js'],
+					src: ['sources/components/**/*.js'],
 					options: {
 						destination: 'doc',
 						template : 'node_modules/ink-docstrap/template',
@@ -71,36 +90,30 @@
 					}
 				}
 			},
-			jshint: {
-				options: {
-					jshintrc: true
-				},
-				js: {
-					files: {
-						src: ['sources/**/*.js', 'tests/**/*.js']
-					}
-				}
-			},
 			watch: {
 				scss: {
-					files: ['sources/sass/**/*.scss'],
+					files: [
+						'sources/**/*.scss',
+						'component-helpers/sass/**/*.scss',
+						'!component-helpers/sass/tmp_styles.scss',
+					],
 					tasks: ['build']
 				},
 				assemble: {
-					files: ['sources/**/*.{hbs,json}', 'component-helpers/assemble/**/*.hbs'],
+					files: ['sources/components/**/*.{hbs,json}', 'component-helpers/assemble/**/*.hbs'],
 					tasks: ['assemble']
 				},
 				jshint: {
-					files: ['sources/js/**/*.js', 'tests/**/*.js'],
+					files: ['sources/components/**/*.js', 'tests/**/*.js'],
 					tasks: ['jshint']
 				},
 			},
 			assemble: {
 				options: {
-					data: 'sources/**/*.{json,yml}',
+					data: 'sources/components/**/*.{json,yml}',
 					helpers: ['component-helpers/assemble/helper/*.js'],
 					layoutdir: 'component-helpers/assemble/layouts/',
-					partials: ['sources/**/*.hbs']
+					partials: ['sources/components/**/*.hbs']
 				},
 				dev: {
 					options: {
@@ -112,73 +125,23 @@
 							expand: true,
 							flatten: true,
 							src: [
-								'component-helpers/assemble/pages/**/*.hbs',
-								'sources/assemble/pages/**/*.hbs'
+								'component-helpers/assemble/pages/**/*.hbs'
 							]
 						}
 					]
 				}
 			},
-			copy: {
-				js: {
-					cwd: 'component-helpers/js/',
-					dest: 'dist/js',
-					expand: true,
-					src: ['**/*.js']
-				}
-			},
 			scssglobbing: {
 				main: {
 					files: {
-						src:"component-helpers/sass/__*.scss"
+						src:"component-helpers/sass/__styles.scss"
 					}
 				}
 			}
 		});
 
-		grunt.registerTask( 'css', ['scssglobbing',  'sass', 'autoprefixer', 'clean:scssglobbing']);
-		grunt.registerTask('build', [ 'clean:dist', 'copy:js', 'css', 'assemble']);
+		grunt.registerTask( 'css', ['scssglobbing', 'sass', 'autoprefixer', 'clean:scssglobbing']);
+		grunt.registerTask('build', [ 'auto_install', 'clean:dist', 'css', 'assemble']);
 		grunt.registerTask('default', ['jshint', 'build', 'watch']);
-
-		grunt.registerTask( 'generate-tmp-styles-scss', 'Generate styles tmp file', function() {
-			var resultContent = grunt.file.read( 'component-helpers/sass/styles_config.scss' );
-
-			//get rid of ../../-prefix, since libsass does not support them in @import-statements+includePaths option
-			resultContent = resultContent.replace( /\"\.\.\/\.\.\//g, '"' );
-
-			var importMatches = resultContent.match( /^@import.+\*.*$/mg );
-
-
-			if ( importMatches ) {
-				importMatches.forEach( function(initialMatch) {
-					// remove all " or '
-					var match = initialMatch.replace( /["']/g, '' );
-					// remove the preceeding @import
-					match = match.replace( /^@import/g, '' );
-					// lets get rid of the final ;
-					match = match.replace( /;$/g, '' );
-					// remove all whitespaces
-					match = match.trim();
-
-					// get all files, which match this pattern
-					var files = grunt.file.expand(
-						{
-							'cwd': 'node_modules/rb_layout_defaults/sources/',
-							'filter': 'isFile'
-						},
-						match
-					);
-
-					var replaceContent = [];
-
-					files.forEach( function(matchedFile) {
-						replaceContent.push( '@import "node_modules/rb_layout_defaults/sources/'  + matchedFile + '";' );
-					} );
-
-					resultContent = resultContent.replace( initialMatch, replaceContent.join( "\n" ) );
-				} );
-			}
-			grunt.file.write( 'tmp/styles.scss', resultContent );
-		} );
 	};
 })();
